f55cfd0082ba202b4552a4d46d0f568379a17662
Daniel <dani3669@gmail.com>
2020-11-30 08:25:18 +0100

    🛹Feat: Headline replacement

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -10,7 +10,7 @@ import theCMS from '../cms/netlify/cms'
  */
 export default {
   siteName: 'Universo SAP',
-  tagline: 'A Nuxt.js Theme for Netlify CMS',
+  tagline: 'Un Blog para profesionales de SAP',
   featureImage: '/uploads/home-hero.jpg',
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
