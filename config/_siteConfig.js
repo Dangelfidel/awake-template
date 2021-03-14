43ae42be9008ef3773ebca01e466dbaf13b63493
danfi@alumni.uv.es <daniel.angel@eligamo.com>
2021-03-14 10:47:49 +0100

    🏄🏼‍♂️ Feat: Change disqus account II

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -44,13 +44,13 @@ export default {
   // Card Layout
   cards: {
     imageDimensions: '2x1', // Options: 1x1, 5x4, 4x3, 3x2, 5x3, 16x9, 2x1, 3x1, 4x5, 3x4, 2x3, 3x5, 9x16
-    theme: 'grid' // Options: boxes, grid, image-grid
+    theme: 'boxes' // Options: boxes, grid, image-grid
   },
 
   // Disqus
   disqus: {
     on: true,
-    loadingStrategy: 'lazy', // Options: onload, lazy, button
+    loadingStrategy: 'button', // Options: onload, lazy, button
     siteShortName: 'https-www-universosap-com',
 
   // Newsletter Subscribe
